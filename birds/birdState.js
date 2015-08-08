24b0f715b603ce19b5a45ed82fd2a30f783ab294
Kelsey <kelseysroberts@gmail.com>
2015-08-08 14:52:08 -0400

    rearranged files

--- birds/birdState.js
+++ birds/birdState.js
@@ -1,7 +1,7 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('bird', {
         url: '/',
-        templateUrl: 'bird.html',
+        templateUrl: 'birds/bird.html',
         controller: 'mainCtrl'
     });
 });
