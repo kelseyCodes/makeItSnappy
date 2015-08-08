c155111b828e6ab81607ce2909362c2e57bd804c
Nirmohi <nirmohi0605@gmail.com>
2015-08-08 12:55:30 -0400

    modified gest.js

--- canvas.js
+++ canvas.js
@@ -1,7 +1,7 @@
-window.app = angular.module('makeItSnappy',[]);
+window.app = angular.module('makeItSnappy', []);
 
-Array.prototype.randomElement =	function(){
-	return this[Math.floor(Math.random()*this.length)]
+Array.prototype.randomElement = function() {
+	return this[Math.floor(Math.random() * this.length)]
 }
 
 app.controller('mainCtrl', function($scope, $window, sound) {
@@ -12,48 +12,48 @@ app.controller('mainCtrl', function($scope, $window, sound) {
 
 	function setupRecorder(stream, callback) {
 		console.log("recorindg stream", stream);
-      var AudioContext = window.AudioContext || window.webkitAudioContext;
-                        audioContext = new AudioContext();
-                        volume = audioContext.createGain(); // creates a gain node
-                        audioInput = audioContext.createMediaStreamSource(stream); // creates an audio node from the mic stream
-                        audioInput.connect(volume); // connect the stream to the gain node
-                        recorder = audioContext.createScriptProcessor(2048, 1, 1);
+		var AudioContext = window.AudioContext || window.webkitAudioContext;
+		audioContext = new AudioContext();
+		volume = audioContext.createGain(); // creates a gain node
+		audioInput = audioContext.createMediaStreamSource(stream); // creates an audio node from the mic stream
+		audioInput.connect(volume); // connect the stream to the gain node
+		recorder = audioContext.createScriptProcessor(2048, 1, 1);
 
-                        var start = Date.now();
+		var start = Date.now();
 
-                        recorder.onaudioprocess = function(stream) {
-                            // if (!recording) return;
-                            //console.log("recording", Date.now() - start);
-                            var left = stream.inputBuffer.getChannelData(0);
-                            //var right = e.inputBuffer.getChannelData(1);
-                            callback(new Float32Array(left));
-                        };
-                        volume.connect(recorder); // connect the recorder
+		recorder.onaudioprocess = function(stream) {
+			// if (!recording) return;
+			//console.log("recording", Date.now() - start);
+			var left = stream.inputBuffer.getChannelData(0);
+			//var right = e.inputBuffer.getChannelData(1);
+			callback(new Float32Array(left));
+		};
+		volume.connect(recorder); // connect the recorder
 
-                        var gainNode = audioContext.createGain();
-                        // debugger;
-                        gainNode.gain.value = 0;
-                        recorder.connect(gainNode);
-                        gainNode.connect(audioContext.destination);
+		var gainNode = audioContext.createGain();
+		// debugger;
+		gainNode.gain.value = 0;
+		recorder.connect(gainNode);
+		gainNode.connect(audioContext.destination);
 
 
 	}
-	gest.start(setupRecorder, 
-		function(data){
-      		if(sound.detectClap(data)){
-        		angular.element('#canvas').trigger('click');
-      		}
+	gest.start(setupRecorder,
+		function(data) {
+			if (sound.detectClap(data)) {
+				angular.element('#canvas').trigger('click');
+			}
 
-      	});
+		});
 
-	  // $scope.recording = new sound.Recording(function(data){
-   //    	if(sound.detectClap(data)){
-   //      		angular.element('#canvas').trigger('click');
-   //    	 }
-   //    });
+	// $scope.recording = new sound.Recording(function(data){
+	//    	if(sound.detectClap(data)){
+	//      		angular.element('#canvas').trigger('click');
+	//    	 }
+	//    });
 
 
-	var rateArr = [4,7,5,6,3];
+	var rateArr = [4, 7, 5, 6, 3];
 	var rate;
 	var colorArr = ["blue", "red", "green", "orange"];
 	var color;
@@ -61,39 +61,36 @@ app.controller('mainCtrl', function($scope, $window, sound) {
 	var fire;
 
 	$scope.clickCoords = function() {
-		 var coordinates = [];
-		 var targetCoords = [];
-			
+		var coordinates = [];
+		var targetCoords = [];
 
-		 coordinates.push(
-	     	$scope.marble.offsetLeft, $scope.marble.offsetTop
-	     )
-	     
-	       
-	     targetCoords.push(
-	     	$scope.goal.offsetLeft, $scope.goal.offsetTop
-	     )
 
-		 if(coordinates[0] < targetCoords[0] + 20 
-		 	&& coordinates[0] > targetCoords[0] - 20
-		 	&& coordinates[1] < targetCoords[1] + 20
-			&& coordinates[1] > targetCoords[1] - 20) {
-		 	$scope.score += 10;
-		 } else {
-		 	$scope.score -= 10;
-		 }
+		coordinates.push(
+			$scope.marble.offsetLeft, $scope.marble.offsetTop
+		)
 
-		 if ($scope.score < -40) {
-		 	$scope.marble.className = "marbleDone";
-		 	$scope.done = true;
-		 	$scope.score = -50;
-		 }
-	
+
+		targetCoords.push(
+			$scope.goal.offsetLeft, $scope.goal.offsetTop
+		)
+
+		if (coordinates[0] < targetCoords[0] + 20 && coordinates[0] > targetCoords[0] - 20 && coordinates[1] < targetCoords[1] + 20 && coordinates[1] > targetCoords[1] - 20) {
+			$scope.score += 10;
+		} else {
+			$scope.score -= 10;
+		}
+
+		if ($scope.score < -400) {
+			$scope.marble.className = "marbleDone";
+			$scope.done = true;
+			$scope.score = -50;
+		}
+
 	};
 
-	gest.options.subscribeWithCallback(function(gesture){
-		if(gesture.right) $scope.goal.style.right = '45%';
-		if(gesture.left) $scope.goal.style.right = '75%';
+	gest.options.subscribeWithCallback(function(gesture) {
+		if (gesture.right) $scope.goal.style.right = '45%';
+		if (gesture.left) $scope.goal.style.right = '75%';
 	})
 
 	$scope.arrows = function($event) {
@@ -103,27 +100,24 @@ app.controller('mainCtrl', function($scope, $window, sound) {
 	}
 
 
-	
 
-		 $scope.marble.addEventListener('webkitAnimationIteration', function(){;
-		 		
-		 		rate = rateArr.randomElement();
-		 		color = colorArr.randomElement();
-		 		fire = fireArr.randomElement();
+	$scope.marble.addEventListener('webkitAnimationIteration', function() {;
 
-		 		$scope.goal.style.borderColor = color;
-		 		$scope.goal.style.right = "60%";
+		rate = rateArr.randomElement();
+		color = colorArr.randomElement();
+		fire = fireArr.randomElement();
 
-		 		$scope.marble.style.webkitAnimationDuration = rate + "s"
-		 		$scope.marble.style.right = fire + "%";
-		 		$scope.marble.style.backgroundColor = color;
+		$scope.goal.style.borderColor = color;
+		$scope.goal.style.right = "60%";
 
-		 })
+		$scope.marble.style.webkitAnimationDuration = rate + "s"
+		$scope.marble.style.right = fire + "%";
+		$scope.marble.style.backgroundColor = color;
 
-	$scope.reloadMe = function(){ 
+	})
+
+	$scope.reloadMe = function() {
 		$window.location.reload();
 	}
 
 });
-
-
